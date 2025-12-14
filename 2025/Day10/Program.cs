006707b24b378a49f0cc37eea4cebfbd6c27ee16
onefangg <onefangg@gmail.com>
2025-12-14 17:15:01 +0800

    Giving up :( Using z3

--- 2025/Day10/Program.cs
+++ 2025/Day10/Program.cs
@@ -34,16 +34,213 @@ for (int i = 0; i < inputData.Length; i++)
 }
 
 
-var partOne = 0;
 // THIS IS SO SLOW >:(
-for (int i = 0; i < machines.Count; i++)
-{
-    Console.WriteLine($"machine {i}: {machines[i]}");
-    partOne += SolveMachine(machines[i]);
-}
+// var partOne = 0;
+// for (int i = 0; i < machines.Count; i++)
+// {
+//     Console.WriteLine($"machine {i}: {machines[i]}");
+//     partOne += SolveMachine(machines[i]);
+// }
 
-Console.WriteLine($"Part 1: {machines.Select(SolveMachine).Sum()}");
+// Console.WriteLine($"Part 1: {machines.Select(SolveMachine).Sum()}");
 
+
+
+
+
+// Console.WriteLine($"Part 2: {SolveSwitchesForMachine(machines[0])}");
+// int SolveSwitchesForMachine(Machine machine)
+// {
+//     var bulbs = machine.Joltage.Length;
+//     var cntMatrix = new int[bulbs][];
+//     for (int r = 0; r < bulbs; r++)
+//     {
+//         cntMatrix[r] = new int[machine.Switches.Count];
+//     }
+//     
+//
+//     for (int c = 0; c < machine.Switches.Count; c++)
+//     {
+//         var switches = machine.Switches[c];
+//         foreach (var sw in switches)
+//         {
+//             cntMatrix[sw][c] = 1;
+//         }
+//     }
+//     
+//     
+//     
+//     return GaussianJordanElimination(cntMatrix, machine.Joltage, machine);
+// }
+//
+//
+//
+// void GaussianElimination(int[][] matrix, int[] v)
+// {
+//     var rows = matrix.Length;
+//     var cols = matrix[0].Length;
+//     for (int idx = 0; idx < rows - 1; idx++)
+//     {
+//         var pivot = -1;
+//         for (int c = 0; c < cols; c++)
+//         {
+//             for (int r = idx; r < rows; r++)
+//             {
+//                 if (matrix[r][c] == 1)
+//                 {
+//                     pivot = c;
+//                     break;
+//                 }
+//             }
+//
+//             if (pivot != -1) break;
+//         }
+//         if (pivot == -1) continue; // ie. it's already in reduced-form
+//         var fromIdx = Enumerable.Range(idx, rows-idx).FirstOrDefault(i => matrix[i][pivot] == 0);
+//         var toIdx = Enumerable.Range(idx, rows-idx).FirstOrDefault(i => matrix[i][pivot] == 1);
+//         if (fromIdx == null || toIdx == null || toIdx < fromIdx) continue; // no need, proceed 
+//         
+//         (matrix[fromIdx], matrix[toIdx]) = (matrix[toIdx], matrix[fromIdx]);
+//         (v[fromIdx], v[toIdx]) = (v[toIdx], v[fromIdx]);
+//         // for (int r = toIdx + 1; r < rows; r++)
+//         // {
+//         //     if (matrix[r][c] != 0)
+//         //     {
+//         //         matrix[r] = matrix[r].Select((val, i) => val - matrix[toIdx][i]).ToArray();
+//         //         v[r] -= v[toIdx];
+//         //     }
+//         //
+//         // }
+//     }
+//
+// }
+//
+// int GaussianJordanElimination(int[][] matrix, int[] v, Machine machine)
+// {
+//     GaussianElimination(matrix, v);
+//     var rows = matrix.Length;
+//     var cols = matrix[0].Length;
+//     // for (int i = 0; i < rows; i++)
+//     // {
+//     //     for (int j = 0; j < cols; j++)
+//     //     {
+//     //         Console.Write(matrix[i][j]);
+//     //     }
+//     //     Console.WriteLine();
+//     // }
+//     // Console.WriteLine("====");
+//
+//     for (int r = rows - 1; r > 0; r--)
+//     {
+//         // working backwards, we start from the last pivot column
+//         int pivot = -1;
+//         for (int c = 0; c< cols; c++)
+//         {
+//             if (matrix[r][c] == 1)
+//             {
+//                 pivot = c;
+//                 break;
+//             }
+//         }
+//
+//         if (pivot == -1) continue;
+//
+//         for (int idx = r - 1; idx >= 0; idx--)
+//         {
+//             if (matrix[idx][pivot] == 0) continue;
+//             matrix[idx] = matrix[idx].Select((x, i) => x - matrix[r][i]).ToArray();
+//             v[idx] -= v[r];
+//         }
+//     }
+//     var freeVariables = new List<int>();
+//     for (int c = 0; c < cols; c++)
+//     {
+//         if (Enumerable.Range(0, rows).Count(x => matrix[x][c] != 0) > 1)
+//         {
+//             freeVariables.Add(c);
+//         }  
+//     }
+//
+//     var maxIter = machine.Joltage.Max();
+//     
+//     
+//     // generate a bunch of free variables in a [col x 1] convention
+//     var allPossibleV = Generate(cols, freeVariables.ToArray(), 0, maxIter);
+//     
+//     // do matrix multiplcation
+//     
+//     // for (int i = 0; i <= maxIter; i++)
+//     // {
+//     //     for (int r = 0; r < rows; r++)
+//     //     {
+//     //         
+//     //         foreach (var freeIdx in freeVariables)
+//     //         {
+//     //             scale[freeIdx] = i;
+//     //         }
+//     //         
+//     //     }
+//     //
+//     // }
+//     // for (int i = 0; i < rows; i++)
+//     // {
+//     //     for (int j = 0; j < cols; j++)
+//     //     {
+//     //         Console.Write(matrix[i][j]);
+//     //     }
+//     //     Console.WriteLine();
+//     // }
+//     //
+//     // for (int i = 0; i < v.Length; i++)
+//     // {
+//     //     Console.Write($"{v[i]} ,");
+//     // }
+//
+//
+//     int[] result = [];
+//     foreach (var scale in allPossibleV)
+//     {
+//         
+//         result = new int[rows];
+//         for (int r = 0; r < rows; r++)
+//         {
+//             result[r] = matrix[r].Select((r, i) => r * scale[i]).Sum();
+//             if (result[r] != v[r]) break;  // doesn't match
+//         }
+//     }
+//     
+//     if (result.Length >0 ) return result.Sum();
+//     throw new Exception("Unable to solve");
+//     
+//
+// }   
+// IEnumerable<int[]> Generate(
+//     int length,
+//     int[] indices,
+//     int n,
+//     int m,
+//     int defaultValue = 0)
+// {
+//     int k = indices.Length;
+//     int range = m - n + 1;
+//
+//     int total = (int)Math.Pow(range, k);
+//
+//     for (int mask = 0; mask < total; mask++)
+//     {
+//         var arr = Enumerable.Repeat(defaultValue, length).ToArray();
+//
+//         int x = mask;
+//         for (int i = 0; i < k; i++)
+//         {
+//             int value = n + (x % range);
+//             arr[indices[i]] = value;
+//             x /= range;
+//         }
+//
+//         yield return arr;
+//     }
+// }
 int SolveMachine(Machine machine)
 {
     // from, to
@@ -120,7 +317,6 @@ int SolveMachine(Machine machine)
 
     return distance[finalState];
 }
-
 
 record LightState
 {
--- 2025/Day12/Program.cs
+++ 2025/Day12/Program.cs
@@ -1,2 +1,67 @@
-﻿var inputData = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sample.txt"));
-// var inputData = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day12.txt"));
+﻿// var inputData = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sample.txt"));
+var inputData = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day12.txt"));
+var sepIdx = Array.FindLastIndex(inputData, x => x.Length == 0);
+
+var shapes = String.Join("", inputData.Take(sepIdx).Select(x =>
+    {
+        if (x == "") return "/n";
+        return x;
+    })).Split("/n")
+    .Select(x =>
+    {
+        var parts = x.Split(':');
+        var grid = new char[3][];
+        grid[0] = parts[1][..3].ToCharArray();
+        grid[1] = parts[1][3..6].ToCharArray();
+        grid[2] = parts[1][6..9].ToCharArray();
+        
+        return new Shape { Index = int.Parse(parts[0]), Grid = grid};
+
+    }).ToArray();
+    
+var trees = inputData.Skip(sepIdx+1).Take(inputData.Length - sepIdx).Select(x =>
+{
+    var parts = x.Split(": ");
+    return new Tree(parts[0], parts[1]);
+
+}).ToArray(); 
+
+var partOne = 0;
+foreach (var t in trees)
+{
+    // doing the cheese solution
+    var area = t.Bound.Item1 * t.Bound.Item2;
+    var requiredShapes = t.Regions.Select((r, i) => (r, i)).Where(x => x.Item1 > 0).ToDictionary(
+        x=>x.i, x=>x.r);
+
+    var shapeArea = 0;
+    foreach (var (idx, cnt) in requiredShapes)
+    {
+        // shapeArea += cnt * shapes.Single(x => x.Index == idx).OccupiedSpace;
+        shapeArea += cnt * 9;
+    }
+
+    if (shapeArea <= area) partOne++;
+    
+}
+Console.WriteLine($"Part 1: {partOne}");
+
+record Shape
+{
+    public int Index { get; set; }
+    public char[][] Grid { get; set; }
+    public int OccupiedSpace => Grid.SelectMany(x => x).Count(x => x =='#');
+}
+
+record Tree
+{
+    public Tree(string bound, string regions)
+    {
+        var b = bound.Split("x").Select(int.Parse).ToArray();
+        Bound = (b[0], b[1]);
+        Regions = regions.Split(" ").Select(int.Parse).ToArray();
+    }
+    
+    public (int, int) Bound { get; set; } 
+    public int[] Regions { get; set; } 
+}
